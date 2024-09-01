63be1bcb22689bc4bb319779695fed54ed14a309
Vamshi <pvmsikrsna@gmail.com>
2024-09-01 19:03:23 +0530

    Try some hack around preamble error

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,8 +1,10 @@
 // generate stub index.html files for dev entry
-import { execSync } from 'child_process'
+import {execSync} from 'child_process'
 import fs from 'fs-extra'
 import chokidar from 'chokidar'
-import { r, port, isDev, log } from './utils'
+import {r, port, isDev, log} from './utils'
+import viteReact from "@vitejs/plugin-react";
+import preambleCode = viteReact.preambleCode;
 
 /**
  * Stub index.html to use Vite in development
@@ -13,20 +15,25 @@ async function stubIndexHtml() {
     'popup',
     'sidepanel',
   ]
-
   for (const view of views) {
     await fs.ensureDir(r(`extension/dist/${view}`))
     let data = await fs.readFile(r(`src/${view}/index.html`), 'utf-8')
     data = data
       .replace('"./main.tsx"', `"http://localhost:${port}/${view}/main.tsx"`)
       .replace('/<div id="(.+?)"></div>/', `<div id="$1">Vite server did not start. Check port ${port}</div>`)
+
+    //--- preamble fix
+    let fixPreambleErrorCode = `<script type="module" src="http://localhost:${port}/fix-preamble.js"></script>`
+    let addPreambleFix = (data: string) => data.replace('</head>', ` ${fixPreambleErrorCode} </head>`)
+    data = addPreambleFix(data)
+    // --- end of preamble fix
     await fs.writeFile(r(`extension/dist/${view}/index.html`), data, 'utf-8')
-    log('PRE', `stub ${view}`)
+    log('PRE', `stub ${view} \n ${data}`)
   }
 }
 
 function writeManifest() {
-  execSync('npx esno ./scripts/manifest.ts', { stdio: 'inherit' })
+  execSync('npx esno ./scripts/manifest.ts', {stdio: 'inherit'})
 }
 
 writeManifest()
--- src/manifest.ts
+++ src/manifest.ts
@@ -60,7 +60,7 @@ export async function getManifest() {
     content_security_policy: {
       extension_pages: isDev
         // this is required on dev for Vite script to load
-        ? `script-src \'self\' http://localhost:${port}; object-src \'self\'`
+        ? `script-src \'self\' http://localhost:${port}; script-src-elem \'self\' http://localhost:${port}; object-src \'self\'`
         : 'script-src \'self\'; object-src \'self\'',
     },
   }
