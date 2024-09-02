a3e80f476dacedd98e730f7c975de53e4445acb4
Vamshi <pvmsikrsna@gmail.com>
2024-09-02 15:30:34 +0530

    Simplify setupTests

--- setupTests.ts
+++ setupTests.ts
@@ -1,10 +1,5 @@
-import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/vitest';
 import * as matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
-// import matchers from '@testing-library/jest-dom/matchers';
+import {expect} from 'vitest';
 
 expect.extend(matchers);
 console.log(`Extended the matchers`)
-
-// == you should add the following import: ==
